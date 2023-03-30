fe9367c6c4600c18b30b2651fc3fe76254ebc52a
Franklin Gutierrez <54650261+Franklp25@users.noreply.github.com>
2023-03-30 11:26:44 -0600

    Update ListaCliente.jsx

--- src/components/Clientes/ListaCliente.jsx
+++ src/components/Clientes/ListaCliente.jsx
@@ -6,20 +6,7 @@ import Table from "../Table.jsx";
 import EditModal from "../modales/EditModal";
 
 const ListaCliente = () => {
-    // let client = {};
-
     const [clients, setClients] = useState([]);
-
-    // const [tipoCedula, setTipoCedula] = useState("");
-    // const [cedula, setCedula] = useState("");
-    // const [nombre, setNombre] = useState("");
-    // const [apellidos, setApellidos] = useState("");
-    // const [telefono, setTelefono] = useState("");
-    // const [email, setEmail] = useState("");
-    // const [direccion, setDireccion] = useState("");
-
-    // const [showEditModal, setShowEditModal] = useState(false);
-    // const [showDeleteModal, setShowDeleteModal] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -28,178 +15,11 @@ const ListaCliente = () => {
 
                 setClients(response.data);
             } catch (error) {
-                // Swal.fire({
-                //     position: "center",
-                //     icon: "error",
-                //     title: "¡Ocurrió un problema, por favor inténtelo de nuevo!",
-                //     showConfirmButton: false,
-                //     timer: 2000,
-                // }).then(() => {
-                //     window.location.reload();
-                // });
                 console.error(error);
             }
         };
         fetchData();
     }, []);
-
-    // const handleEdit = (event) => {
-    //     setShowEditModal(false);
-    //     event.preventDefault();
-    //     let usuario = {
-    //         _id: id,
-    //         nombre: nombre,
-    //         cedula: cedula,
-    //         correo: correo,
-    //         contrasena: contrasena,
-    //         rol: rol,
-    //     };
-    //     let message =
-    //         "Lo sentimos, ocurrió un problema durante el proceso, por favor inténtelo de nuevo";
-    //     let icon = "error";
-    //     axios
-    //         .post(`/aqua-system/user/update/${id}`, usuario)
-    //         .then((res) => {
-    //             if (res.data.data !== "") {
-    //                 message = res.data.data;
-    //                 icon = "success";
-    //             }
-    //             if (res.status === 200) {
-    //                 message = "¡Usuario actualizado exitosamente!";
-    //                 icon = "success";
-    //             } else if (res.status === 400) {
-    //                 message =
-    //                     "¡Error, ya existe un usuario con ese número de cédula!";
-    //             }
-    //             Swal.fire({
-    //                 position: "center",
-    //                 icon: icon,
-    //                 title: message,
-    //                 showConfirmButton: false,
-    //                 timer: 2000,
-    //             }).then(() => {
-    //                 window.location.reload();
-    //             });
-    //         })
-    //         .catch((err) => {
-    //             if (err.response) {
-    //                 if (err.response.status === 200) {
-    //                     message = "¡Usuario actualizado exitosamente!";
-    //                     icon = "success";
-    //                 } else if (err.response.status === 400) {
-    //                     message =
-    //                         "¡Error, ya existe un usuario con ese número de cédula!";
-    //                     icon = "error";
-    //                 }
-    //             }
-    //             Swal.fire({
-    //                 position: "center",
-    //                 icon: icon,
-    //                 title: message,
-    //                 showConfirmButton: false,
-    //                 timer: 2000,
-    //             }).then(() => {
-    //                 window.location.reload();
-    //             });
-    //         });
-    // };
-    // const editForm = (
-    //     <>
-    //         <form className="w-full" onSubmit={handleEdit}>
-    //             <div className="  w-full mb-6">
-    //                 <label className="inline-block mb-2">Cédula:</label>
-    //                 <input
-    //                     className="shadow appearance-none border rounded w-full py-2 px-1 text-black"
-    //                     id="cedula"
-    //                     name="cedula"
-    //                     value={cedula}
-    //                     required
-    //                 />
-
-    //                 <label className="inline-block my-2">
-    //                     Nombre completo:
-    //                 </label>
-    //                 <input
-    //                     className="shadow appearance-none border rounded w-full py-2 px-1 text-black"
-    //                     id="nombre"
-    //                     name="nombre"
-    //                     value={nombre}
-    //                     onChange={(e) => {
-    //                         setNombre(e.target.value);
-    //                     }}
-    //                     required
-    //                 />
-    //                 <label className="inline-block my-2">
-    //                     Correo electrónico:
-    //                 </label>
-    //                 <input
-    //                     className="shadow appearance-none border rounded w-full py-2 px-1 text-black"
-    //                     id="correo"
-    //                     name="correo"
-    //                     value={correo}
-    //                     onChange={(e) => {
-    //                         setCorreo(e.target.value);
-    //                     }}
-    //                     required
-    //                 />
-    //                 <label className="inline-block my-2">Contraseña:</label>
-    //                 <input
-    //                     id="contrasena"
-    //                     type="password"
-    //                     name="contrasena"
-    //                     className="shadow border rounded w-full py-2 px-1 text-black"
-    //                     value={contrasena}
-    //                     required
-    //                 />
-
-    //                 <label className="inline-block my-2">Rol:</label>
-    //                 <select
-    //                     id="rol"
-    //                     className="shadow border rounded w-full py-2 px-1 text-black"
-    //                     name="rol"
-    //                     value={rol}
-    //                     onChange={(e) => {
-    //                         setRol(e.target.value);
-    //                     }}
-    //                     required
-    //                 >
-    //                     <option disabled value="">
-    //                         Seleccione el rol que tendrá el usuario dentro del
-    //                         sistema...
-    //                     </option>
-    //                     <option value="usuario">Usuario</option>
-    //                     <option value="administrador">Administrador</option>
-    //                 </select>
-    //             </div>
-    //             <div className="h-auto flex items-center justify-start ">
-    //                 <button
-    //                     className="shrink mr-5 px-3 py-2.5 bg-yellow-500 text-white font-bold  leading-tight uppercase rounded shadow-md hover:bg-yellow-700 hover:shadow-lg focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
-    //                     type="submit"
-    //                 >
-    //                     Editar
-    //                 </button>
-    //                 <button
-    //                     className="shrink  px-3 py-2.5 bg-red-600 text-white font-bold  leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
-    //                     type="button"
-    //                     onClick={() => setShowEditModal(false)}
-    //                 >
-    //                     Cancelar
-    //                 </button>
-    //             </div>
-    //         </form>
-    //     </>
-    // );
-
-    // const handleShowEdit = (cell) => {
-    //     setShowEditModal(true);
-    //     client = cell?.row?.original;
-    //     setCedula(client.cedula);
-    //     setNombre(client.nombre);
-    //     setApellidos(client.apellidos);
-    //     setTelefono(client.telefono);
-    //     setEmail(client.email);
-    //     setDireccion(client.direccion);
-    // };
 
     const columns = [
         {
@@ -237,7 +57,7 @@ const ListaCliente = () => {
                 <button
                     type="button"
                     title="EditClient"
-                    // className="text-yellow-500 hover:text-yellow-700 text-xl ml-4"
+                    className=""
                     onClick={() => handleShowEdit(props)}
                 >
                     <svg
@@ -292,13 +112,6 @@ const ListaCliente = () => {
             <h1 className=" text-gray-600 p-5 font-bold text-2xl pl-6 ">
                 Lista de Clientes
             </h1>
-            {/* {showEditModal ? (
-                <EditModal
-                    showModal={showEditModal}
-                    setShowModal={setShowEditModal}
-                    form={editForm}
-                />
-            ) : null} */}
             <Table columns={columns} data={clients} />
         </>
     );
