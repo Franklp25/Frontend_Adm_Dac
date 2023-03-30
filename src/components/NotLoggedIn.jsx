c470ebad859c75dd22179a73b84b28c788352fc1
Franklin Gutierrez <54650261+Franklp25@users.noreply.github.com>
2023-03-30 10:25:30 -0600

    Update NotLoggedIn.jsx

--- src/components/NotLoggedIn.jsx
+++ src/components/NotLoggedIn.jsx
@@ -1,7 +1,7 @@
 export default function NotLoggedIn() {
     return (
         <>
-            {window.location.href !== "http://127.0.0.1:5173/" ? ( //ruta donde se haga deploy
+            {window.location.href !== "https://admdacbiogen.netlify.app" ? ( //ruta donde se haga deploy
                 <div className="flex absolute w-full justify-center bg-gray-800 h-full">
                     <h1 className="bg-yellow-200 rounded-3xl p-4 font-bold h-fit text-xl">
                         POR FAVOR INICIE SESIÓN PARA ACCEDER AL SISTEMA
