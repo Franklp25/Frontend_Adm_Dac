1ee882e2b505fb3ea31bf327012d6a878aef76a2
Franklin Gutierrez <54650261+Franklp25@users.noreply.github.com>
2023-04-17 23:09:38 -0600

    Update AgregarFacturasPagar.jsx

--- src/components/CuentasPagar/AgregarFacturasPagar.jsx
+++ src/components/CuentasPagar/AgregarFacturasPagar.jsx
@@ -1,5 +1,14 @@
+import { useState } from "react";
+import Navbar from "../Navbar";
+import Alerta from "../Alerta.jsx";
+import clienteAxios from "../../config/clienteAxios";
+import Swal from "sweetalert2";
+
 const AgregaFacturasPagar = () => {
-    return <div>AgregarFacturasPagar</div>;
+    const [numFacturaPagar, setFacturaPagar] = useState("");
+    const [fechaEmision, setFechaEmision] = useState("");
+    const [diasCredito, setDiasCreditp] = useState("");
+    return <></>;
 };
 
 export default AgregaFacturasPagar;
