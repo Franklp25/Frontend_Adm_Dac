082608bdd98a5e934ed52e6969e9efca16ea724b
Jhongx <jhongx@live.com>
2023-05-08 04:39:04 -0600

    Avance del agregar factura

--- src/components/CuentasCobrar/AgregarFacturasCobrar.jsx
+++ src/components/CuentasCobrar/AgregarFacturasCobrar.jsx
@@ -22,16 +22,74 @@ import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import AddDeleteTableRows from "./table/AddDeleteTableRows.jsx";
 
-
 const AgregarFacturasCobrar = () => {
+    const [fechaEmision, setFechaEmision] = useState("");
+    const [diasCredito, setDiasCredito] = useState("");
+    const [fechaVencimiento, setFechaVencimiento] = useState("");
     return (
         <>
             <Navbar />
+
             <div className="flex justify-between p-2">
                 <h1 className=" text-gray-600 p-5 font-bold text-2xl pl-6 ">
                     Agregar Factura Por Cliente
                 </h1>
             </div>
+            <div className="flex flex-row items-center justify-center px-6 py-8 space-x-4">
+                <div>
+                    <label
+                        htmlFor="fechaEmision"
+                        className="uppercase text-gray-600 block text-sm font-bold pb-2 text-center"
+                    >
+                        Fecha Emitida
+                    </label>
+                    <input
+                        type="date"
+                        id="fechaEmision"
+                        className="border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-32 md:w-48 p-2.5 dark:bg-blue dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        required=""
+                        value={fechaEmision}
+                        onChange={(e) => setFechaEmision(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <div>
+                        <label
+                            htmlFor="diasCredito"
+                            className="uppercase text-gray-600 block text-sm font-bold pb-2 text-center"
+                        >
+                            Días de crédito
+                        </label>
+
+                        <input
+                            type="number"
+                            id="diasCredito"
+                            // placeholder="Escriba nombre del cliente"
+                            className="border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-blue dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            required=""
+                            value={diasCredito}
+                            onChange={(e) => setDiasCredito(e.target.value)}
+                        />
+                    </div>
+                </div>
+                <div>
+                    <label
+                        htmlFor="fechaVencimiento"
+                        className="uppercase text-gray-600 block text-sm font-bold pb-2 text-center"
+                    >
+                        Fecha Vencimiento
+                    </label>
+                    <input
+                        type="date"
+                        id="fechaVencimiento"
+                        className="border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-32 md:w-48 p-2.5 dark:bg-blue dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        required=""
+                        value={fechaVencimiento}
+                        onChange={(e) => setFechaVencimiento(e.target.value)}
+                    />
+                </div>
+            </div>
+
             <AddDeleteTableRows />
         </>
     );
--- src/components/CuentasCobrar/table/TableRows.jsx
+++ src/components/CuentasCobrar/table/TableRows.jsx
@@ -1,38 +1,44 @@
-function TableRows({ rowsData, deleteTableRows, handleChange }) {
+function TableRows({ rowsData, deleteTableRows, productos,productoSeleccionado, handleChange }) {
     return rowsData.map((data, index) => {
-        const { fullName, emailAddress, salary } = data;
+        const { producto, precio, cantidad } = data;
         return (
-            <tr key={index}>
-                <td>
-                    <input
-                        type="text"
-                        value={fullName}
+            <tr key={index} className="bg-gray-100 hover:bg-gray-200">
+                <td className="p-2">
+                    <select
+                        value={producto}
                         onChange={(evnt) => handleChange(index, evnt)}
-                        name="fullName"
-                        className="form-control"
+                        name="producto"
+                        className="px-1 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+                    >
+                        <option value="">Selecciona un producto</option>
+                        {productos.map((prod) => (
+                            <option key={prod.id} value={prod.nombre}>
+                                {prod.nombre}
+                            </option>
+                        ))}
+                    </select>
+                </td>
+                <td className="p-2">
+                    <input
+                        type="number"
+                        value={precio}
+                        onChange={(evnt) => handleChange(index, evnt)}
+                        name="precio"
+                        className="px-1 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
                     />
                 </td>
-                <td>
+                <td className="p-2">
                     <input
-                        type="text"
-                        value={emailAddress}
+                        type="number"
+                        value={cantidad}
                         onChange={(evnt) => handleChange(index, evnt)}
-                        name="emailAddress"
-                        className="form-control"
-                    />{" "}
+                        name="cantidad"
+                        className="px-1 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+                    />
                 </td>
-                <td>
-                    <input
-                        type="text"
-                        value={salary}
-                        onChange={(evnt) => handleChange(index, evnt)}
-                        name="salary"
-                        className="form-control"
-                    />{" "}
-                </td>
-                <td>
+                <td className="p-2">
                     <button
-                        className="btn btn-outline-danger"
+                        className="px-4 py-1 text-white bg-red-500 rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50"
                         onClick={() => deleteTableRows(index)}
                     >
                         x
