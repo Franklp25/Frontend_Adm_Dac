29afe1cf17e147b4771c2df98a8cce659aa6b811
Jhongx <jhongx@live.com>
2023-05-12 09:33:58 -0600

    Frank

--- src/components/CuentasPagar/AgregarFacturasPagar.jsx
+++ src/components/CuentasPagar/AgregarFacturasPagar.jsx
@@ -214,10 +214,6 @@ const AgregaFacturasPagar = () => {
                                         id="fechaVencimiento"
                                         // placeholder="Escriba nombre del cliente"
                                         className=" border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-blue dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-<<<<<<< HEAD
-                                        required=""
-=======
->>>>>>> Frank
                                         value={fechaVencimiento}
                                         onChange={(e) =>
                                             setFechaVencimiento(e.target.value)
