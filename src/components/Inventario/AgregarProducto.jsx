56832d85d16580c0aa28c7ff5c1690d3b4904768
Franklin Gutierrez <54650261+Franklp25@users.noreply.github.com>
2023-05-15 02:44:13 -0600

    Update AgregarProducto.jsx

--- src/components/Inventario/AgregarProducto.jsx
+++ src/components/Inventario/AgregarProducto.jsx
@@ -61,15 +61,15 @@ const AgregarProducto = () => {
                 Agregar Producto
             </h1>
 
-            <section className="">
-                <div className="flex flex-col items-center justify-center px-6 py-8">
+            <section className="px-4 py-8">
+                <div className="flex flex-col items-center justify-center md:flex md:flex-col md:items-center md:justify-center px-6 py-8">
                     <div className="w-full bg-slate-50 rounded-2xl shadow-xl dark:border md:w-7/12 sm:w-2/3 xl:w-2/4 :2-3/">
-                        <div className="p-6 ">
+                        <div className=" p-6 space-y-6 md:p-6 ">
                             <form
-                                className="grid grid-cols-3 gap-4 md:gap-6"
+                                className="grid grid-cols-1 gap-4 md:grid md:grid-cols-3 md:gap-6"
                                 onSubmit={handleSubmit}
                             >
-                                <div className="col-span-1">
+                                <div className="md:col-span-1">
                                     <label
                                         htmlFor="codigo"
                                         className=" uppercase text-gray-600 block text-sm font-bold pb-2"
@@ -81,7 +81,7 @@ const AgregarProducto = () => {
                                         type="text"
                                         id="codigo"
                                         placeholder="Escriba nombre del producto"
-                                        className=" border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-blue dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        className="block w-full px-4 py-2 mt-1 md:border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 md:block md:w-full p-2.5 dark:bg-blue dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         required=""
                                         value={codigo}
                                         onChange={(e) =>
@@ -89,7 +89,7 @@ const AgregarProducto = () => {
                                         }
                                     />
                                 </div>
-                                <div className="col-span-1">
+                                <div className="md:col-span-1">
                                     <label
                                         htmlFor="nombre"
                                         className=" uppercase text-gray-600 block text-sm font-bold pb-2"
@@ -101,7 +101,7 @@ const AgregarProducto = () => {
                                         type="text"
                                         id="nombre"
                                         placeholder="Escriba nombre del producto"
-                                        className=" border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-blue dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        className="block w-full px-4 py-2 mt-1 md:border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 md:block md:w-full p-2.5 dark:bg-blue dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         required=""
                                         value={nombre}
                                         onChange={(e) =>
@@ -109,7 +109,7 @@ const AgregarProducto = () => {
                                         }
                                     />
                                 </div>
-                                <div className="col-span-1">
+                                <div className="md:col-span-1">
                                     <label
                                         htmlFor="precio"
                                         className=" uppercase text-gray-600 block text-sm font-bold pb-2"
@@ -121,7 +121,7 @@ const AgregarProducto = () => {
                                         type="text"
                                         id="precio"
                                         placeholder="Digite el precio"
-                                        className=" border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-blue dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        className="block w-full px-4 py-2 mt-1 md:border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 md:block md:w-full p-2.5 dark:bg-blue dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         required=""
                                         value={precio}
                                         onChange={(e) =>
@@ -129,7 +129,7 @@ const AgregarProducto = () => {
                                         }
                                     />
                                 </div>
-                                <div className="col-span-3">
+                                <div className="md:col-span-3">
                                     <label
                                         htmlFor="descripcion"
                                         className=" uppercase text-gray-600 block text-sm font-bold pb-2"
@@ -141,7 +141,7 @@ const AgregarProducto = () => {
                                             type="text"
                                             id="descripcion"
                                             placeholder="Descripción del producto"
-                                            className=" pr-80 border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-blue dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                            className=" block w-full px-4 py-2 mt-1 border rounded-md focus:outline-none md:pr-80 md:border sm:text-sm md:rounded-lg focus:ring-primary-600 focus:border-primary-600 md:block md:w-full p-2.5 dark:bg-blue dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                             required=""
                                             value={descripcion}
                                             onChange={(e) =>
@@ -150,12 +150,18 @@ const AgregarProducto = () => {
                                         />
                                         <button
                                             type="submit"
-                                            className="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-200 w-full uppercase text-blue  bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-700 dark:hover:bg-primary-700 dark:focus:ring-primary-800 "
+                                            className=" max-[768px]:hidden transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-200 w-full uppercase text-blue  bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-700 dark:hover:bg-primary-700 dark:focus:ring-primary-800 "
                                         >
                                             Guardar Producto
                                         </button>
                                     </div>
                                 </div>
+                                <button
+                                    type="submit"
+                                    className=" md:hidden transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-200 w-full uppercase text-blue  bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-700 dark:hover:bg-primary-700 dark:focus:ring-primary-800 "
+                                >
+                                    Guardar Producto
+                                </button>
                             </form>
                             <div className=" m-2">
                                 {msg && <Alerta alerta={alerta} />}
