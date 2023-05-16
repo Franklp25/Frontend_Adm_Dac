7ee048c64482fe5a09b7043473b51dcc5b434d80
Franklin Gutierrez <54650261+Franklp25@users.noreply.github.com>
2023-05-16 00:23:43 -0600

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,9 +5,14 @@ const Home = () => {
         <>
             <Navbar />
 
-            <h1 className=" text-5xl font-bold p-16 mt-32 text-left ">
-                Sistema Administrador <br /> DAC-BIO&GEN{" "}
+            <h1 class=" mt-20 ml-20 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+                <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+                    DAC Bio&Gen
+                </span>{" "}
             </h1>
+            <p class="ml-20 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+                Bienvenido al Sistema Administrador
+            </p>
         </>
     );
 };
