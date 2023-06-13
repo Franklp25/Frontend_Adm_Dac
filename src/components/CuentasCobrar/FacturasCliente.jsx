14a9c24017fd747a30c598286875646f184679ac
Franklin Gutierrez <54650261+Franklp25@users.noreply.github.com>
2023-06-13 01:42:54 -0600

    Update FacturasCliente.jsx

--- src/components/CuentasCobrar/FacturasCliente.jsx
+++ src/components/CuentasCobrar/FacturasCliente.jsx
@@ -323,7 +323,7 @@ const FacturasCliente = () => {
                         to={`/agregarFacturasCobrar/${params.id}`}
                         className="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-200 p-2  text-white bg-green-600 hover:bg-green-800 rounded-md text-lg font-semibold"
                     >
-                        Agregar Factura Proveedor
+                        Agregar Factura
                     </Link>
                 </div>
             </div>
