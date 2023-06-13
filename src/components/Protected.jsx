2a7a1d5f68f2ab7381756400b4c1f667ac232b4b
Franklin Gutierrez <54650261+Franklp25@users.noreply.github.com>
2023-06-13 01:31:56 -0600

    Update Protected.jsx

--- src/components/Protected.jsx
+++ src/components/Protected.jsx
@@ -1,13 +1,7 @@
-import { useContext } from "react";
-import AuthContext from "../context/AuthProvider";
 import NotLoggedIn from "./NotLoggedIn";
 
 export default function Protected({ children }) {
-    const { auth, loading } = useContext(AuthContext);
-
-    if (loading) {
-        return <div>Loading...</div>; // Puedes reemplazar esto por tu componente de carga.
-    }
-
-    return auth && auth.token ? children : <NotLoggedIn />;
+    let token = null;
+    token = localStorage.getItem("token");
+    return <>{token !== null ? <>{children}</> : <NotLoggedIn />}</>;
 }
